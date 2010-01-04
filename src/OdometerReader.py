a30770e28c84b70bd85d5c8af5825ab4ad30d91a
Thierry Genovese <tgenovese@users.noreply.github.com>
2010-01-04 21:58:33 +0000

    Add an OdometerReader MyHDL module to decode the outputs of a quadrature encoder and keep track of its position in a 16-bit unsigned integer.

---
+++ src/OdometerReader.py
@@ -0,0 +1,48 @@
+from myhdl import Signal, intbv, always, always_comb
+
+LOW, HIGH = bool(0), bool(1)
+
+MAX_COUNT = 2**16
+
+def OdometerReader(count, a, b, clk25, rst_n):
+    """ Quadrature decoder for odometers
+
+    Uses a 25 MHz clock to sample a quadrature encoder's A and B signals
+    and keep count of the number of ticks it made.
+
+    Uses rising and falling edges of both signals for a x4 decoder.
+
+    count -- 16-bit output count, subject to overflow and underflow
+    a -- channel A of the quadrature encoder
+    b -- channel B of the quadrature encoder
+    clk25 -- 25 MHz clock input
+    rst_n -- active low reset input (count is reset when active)
+
+    """
+
+    int_count  = Signal(intbv(0)[16:]) # so that count can be read and set
+    previous_a = Signal(LOW)
+    previous_b = Signal(LOW)
+
+    @always(clk25.posedge, rst_n.negedge)
+    def readOdometer():
+        """ Read quadrature encoder's channels and keep internal count """
+        if rst_n == LOW:
+            int_count.next = 0
+            previous_a.next = a
+            previous_b.next = b
+        else:
+            if a ^ previous_a ^ b ^ previous_b: # it moved
+                if a ^ previous_b: # which direction
+                    int_count.next = (int_count + 1) % MAX_COUNT
+                else:
+                    int_count.next = (int_count - 1) % MAX_COUNT
+            previous_a.next = a
+            previous_b.next = b
+
+    @always_comb
+    def driveOutput():
+        """ Copies the internal count to the count output """
+        count.next = int_count
+
+    return readOdometer, driveOutput
---
+++ src/test_OdometerReader.py
@@ -0,0 +1,72 @@
+import unittest
+
+from myhdl import Signal, intbv, traceSignals, Simulation, delay, always
+
+from OdometerReader import OdometerReader, LOW, HIGH
+
+def ClkGen(clk):
+    """ 25 MHz clock generator.
+    clk -- clock signal
+    """
+    @always(delay(1)) # each delay unit simulates 0.02 us (half-period)
+    def genClk():
+        clk.next = not clk
+    return genClk
+
+def TestBench(OdometerTester):
+    """ Instanciate modules and wire things up.
+    OdometerTester -- test module to instanciate with OdometerReader and ClkGen
+    """
+
+    # create signals with default values
+    count = Signal(intbv(0)[16:])
+    a = Signal(LOW)
+    b = Signal(LOW)
+    clk = Signal(LOW)
+    rst_n = Signal(HIGH)
+
+    # instanciate modules
+    OdometerReader_inst = traceSignals(OdometerReader,
+        count, a, b, clk, rst_n)
+    OdometerTester_inst = OdometerTester(
+        count, a, b, clk, rst_n)
+    ClkGen_inst = ClkGen(clk)
+
+    return OdometerReader_inst, OdometerTester_inst, ClkGen_inst
+
+class TestOdometerReader(unittest.TestCase):
+
+    def testOdometerReader(self):
+        """ Ensures that count works as espected """
+
+        def moveForward(count, a, b, clk, rst_n):
+            for i in range(4):
+                a.next = not a
+                yield delay(10)
+                b.next = not b
+                yield delay(10)
+
+        def keepStill(count, a, b, clk, rst_n):
+            for i in range(80):
+                yield delay(1)
+
+        def moveBackward(count, a, b, clk, rst_n):
+            for i in range(4):
+                b.next = not b
+                yield delay(10)
+                a.next = not a
+                yield delay(10)
+
+        def OdometerTester(count, a, b, clk, rst_n):
+            yield moveForward(count, a, b, clk, rst_n)  # 0 to 8
+            yield keepStill(count, a, b, clk, rst_n)    # 8
+            yield moveBackward(count, a, b, clk, rst_n) # 8 to 0
+            yield moveBackward(count, a, b, clk, rst_n) # 0 to -8 = 0xFFF8
+            yield keepStill(count, a, b, clk, rst_n)    # 0xFFF8
+            yield moveForward(count, a, b, clk, rst_n)  # 0xFFF8 to 0
+
+        sim = Simulation(TestBench(OdometerTester))
+        sim.run(500)
+
+if __name__ == '__main__':
+    unittest.main()
